625e2a95d9d841126759c0387797b8e7e51e2b02
Andrew Gross <apgross@udel.edu>
2016-10-24 17:31:54 -0400

    Created list of tests for Game 1

---
+++ Game1/src/main/test/Game1Tests.java
@@ -0,0 +1,20 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Game1Tests {
+	//List of phenomena that occur in game:
+	//Wave hit (initial)
+	//Wave hit (after round) (Covers BarModel)
+	//Random spawning
+	//Pickup
+	//Game start (tests timer)
+	//Game end (tests timer as well)
+	//Movement of animal
+	
+	@Test
+	public void test() {
+		
+	}
+
+}
