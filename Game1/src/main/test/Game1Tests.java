ee3d17185802b1c08f28f21f627d91811915111c
Andrew Gross <apgross@udel.edu>
2016-10-26 00:04:05 -0400

    Tests added

--- Game1/src/main/test/Game1Tests.java
+++ Game1/src/main/test/Game1Tests.java
@@ -2,6 +2,8 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import model.AnimalModel;
+
 public class Game1Tests {
 	//Low level tests:
 	//
@@ -16,9 +18,12 @@ public class Game1Tests {
 	//Game end (tests timer as well)
 	//Movement of animal
 	
+	
+	//AnimalModel
 	@Test
-	public void test() {
-		
+	public void testPickUp() {
+		AnimalModel myAnimal = new AnimalModel();
+		myAnimal.pickUp(myAnimal.getLocX(), myAnimal.getLocY());
 	}
 
 }
